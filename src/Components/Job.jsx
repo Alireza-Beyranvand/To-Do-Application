5569f9cffe7d2e335fee9b2116677dd7fb8d313e
Alireza Sepahvand <AlirezaSepahvand69@gmail.com>
2024-05-23 01:22:52 +0330

    New Update

--- src/Components/Job.jsx
+++ src/Components/Job.jsx
@@ -10,10 +10,10 @@ import { useDispatch } from "react-redux";
 const Job = ({ AllJobs }) => {
 
 
-const Dispatch = useDispatch();
+  const Dispatch = useDispatch();
 
 
-
+  const StatusJob = AllJobs.status;
 
 
   // State for Change completeJob
@@ -21,24 +21,6 @@ const Dispatch = useDispatch();
     ...AllJobs, status: "Completed"
   })
 
-  
-  // life cycle for Change Style <del></del> 
-  useEffect(() => {
-    if (AllJobs.status === "Completed") {
-      ChangeTag.current.style.display = "inline";
-      DesReal.current.style.display = "none";
-      NameDel.current.style.display = "none";
-      ShowDelName.current.style.display = "inline";
-    }
-    console.log("Render")
-  }, [])
-
-
-  // useRef for Change Tags
-  const ChangeTag = useRef(null);
-  const DesReal = useRef(null);
-  const NameDel = useRef(null);
-  const ShowDelName = useRef(null);
 
 
 
@@ -52,16 +34,9 @@ const Dispatch = useDispatch();
 
 
   // function btn for change style Description
-
   const delJobs = async () => {
-    ChangeTag.current.style.display = "inline";
-    DesReal.current.style.display = "none";
-    NameDel.current.style.display = "none";
-    ShowDelName.current.style.display = "inline";
     await Dispatch(EditJob(preJob));
   };
-
-
 
 
 
@@ -75,10 +50,12 @@ const Dispatch = useDispatch();
         </div>
         <div className="card-body text-center">
 
-          <h5 ref={NameDel} className="card-title" style={{ color: "#DD2D4A", fontWeight: "bold" }}>
+          <h5 className="card-title" style={StatusJob === "Completed" ? { display: "none" }
+            : { display: "inline" } && { color: "#DD2D4A", fontWeight: "bold" }}>
             {AllJobs.name}
           </h5>
-          <del ref={ShowDelName} style={{ display: "none" , fontWeight:"900d" }}>{" "} {AllJobs.name} {" "}  </del>
+          <del style={StatusJob === "Completed" ? { display: "inline" } :
+            { display: "none", fontWeight: "900d" }}>{" "} {AllJobs.name} {" "}  </del>
 
           <div className="container">
             <hr className="hr-text text-dark" />
@@ -86,16 +63,18 @@ const Dispatch = useDispatch();
 
           <p className="card-text" >
             <i className="fa">  Description </i>   <i className="	fa fa-file-text"> : </i>{"   "}
-            <b ref={DesReal}>{AllJobs.Description}</b>
-            <del ref={ChangeTag} style={{ display: "none", fontWeight: "bold" }}>{AllJobs.Description} {"  "}
+            <b style={StatusJob === "Completed" ? { display: "none" } : { display: "inline" }}>{AllJobs.Description}</b>
+            <del style={StatusJob === "Completed" ?
+              { display: "inline" } : { display: "none", fontWeight: "bold" }
+            }>{AllJobs.Description} {"  "}
               <i className="fa fa-check bg-succsess p-2 mx-2" style={{ backgroundColor: "#8ADAB2", borderRadius: "20px 0 15px 0" }}> Completed</i></del>
           </p>
 
           <button className="btn btn-success mt-2" onClick={delJobs}> <i className="fa fa-check" ></i></button>
           <div className="container btn-group w-50 mt-2" >
             <Link to={`/viewjob/${jobId}`} className="btn btn-warning text-center "><i className="fa-solid fa-eye"></i></Link>
-            <button onClick={() => remove(jobId)} 
-            className="btn btn-danger text-dark"
+            <button onClick={() => remove(jobId)}
+              className="btn btn-danger text-dark"
             ><i className="fa-solid fa-trash"></i></button>
           </div>
         </div>
